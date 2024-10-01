75345c959905607d2e82c0dd383cbbb8fe2a5e49
aadii <ad1123itya@gmail.com>
2024-10-01 15:41:45 +0530

    skills add

--- src/components/SkilCard.jsx
+++ src/components/SkilCard.jsx
@@ -8,8 +8,8 @@ const SkilCard = () => {
                 skills.map((skill) => {
                     return (
                         <div className='h-fit w-fit py-1 md:px-4 px-4 rounded-lg bg-white border border-blue-600 flex items-center gap-2' key={skill.id}>
-                            <div className=' h-10 w-10'>
-                                <img className='' src={skill.logo} alt="" />
+                            <div className=' h-10 w-10 rounded-md overflow-hidden'>
+                                <img className='object-cover h-full w-full' src={skill.logo} alt="" />
                             </div>
                             <h1 className='text-slate-800'>{skill.name}</h1>
                         </div>
